317c56bd5eca90c4c35cae769eb414078c114c83
Gohil Jitrajsinh <projectghost15@gmail.com>
2025-08-12 18:00:39 +0530

    Add files via upload

---
+++ problem 3(1).c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+
+{
+    clrscr();
+    int a,b,c;
+    printf("Enter three number  \n");
+
+    scanf("%d%d%d",&a,&b,&c);
+     if (a>b)&&(a>c)
+     {
+      printf(" First number is larger %d",a);
+     }
+     else
+     {
+      printf("others");
+     }
+
+}
