67bf3e42d9d02d2f3e5cde390d01a01912f6d491
Gohil Jitrajsinh <projectghost15@gmail.com>
2025-09-02 17:37:18 +0530

    Add files via upload

---
+++ array problem 1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+    int nums[5]={1,2,3,4,5};
+
+    for (int i=0;i<5;i++)
+             printf("%d",nums[i]);
+              printf("\n");
+
+
+    return 0;
+}
---
+++ array problem 2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+
+    int a,b,c,d,e,f,g,h,i,j;
+    printf("Enter 10 numbers for array : ");
+    scanf("%d %d %d %d %d %d %d %d %d %d",&a,&b,&c,&d,&e,&f,&g,&h,&i,&j);
+    int n[10]={a,b,c,d,e,f,g,h,i,j};
+    for (int i=0;i<10;i++)
+        printf("%d",n[3]);
+        printf("%d",n[6]);
+        printf("%d",n[8]);
+    return 0;
+}
